ade8567fdc892e4e85336dfcf0a863ab4b2703bc
Keagen <keegenjt@gmail.com>
2024-11-18 12:43:55 -0500

    Created flow for categories to sort by type

--- src/components/StoreItem.vue
+++ src/components/StoreItem.vue
@@ -1,7 +1,33 @@
 <template>
-  <!-- your answer -->
+  <v-card>
+    <v-img
+      :src="product.data.image"
+      :alt="product.data.name"
+      height="200"
+    ></v-img>
+    <v-card-title>
+      {{ product.data.name }}
+    </v-card-title>
+    <v-card-subtitle>
+      {{ product.data.category }}
+    </v-card-subtitle>
+    <v-card-text>
+      <p>Price: ${{ product.data.price }}</p>
+      <p>Rating: {{ product.data.rating }}</p>
+      <p>{{ product.data.description }}</p>
+    </v-card-text>
+    <v-card-actions>
+      <v-btn color="primary">Add to Cart</v-btn>
+    </v-card-actions>
+  </v-card>
 </template>
 
 <script lang="ts" setup>
-// your answer
+import { defineProps } from "vue";
+import { ProductDoc } from "../types/product";
+
+defineProps<{
+  product: ProductDoc;
+}>();
 </script>
+
